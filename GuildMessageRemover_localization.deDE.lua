b0126f2585c54284e28a2e848b828c3f1d161f8c
Kristoffer Tverå <Bluekristoffer@gmail.com>
2018-07-27 17:40:08 +0200

    Localization updates for new string needed (global filter)

--- GuildMessageRemover_localization.deDE.lua
+++ GuildMessageRemover_localization.deDE.lua
@@ -1,6 +1,8 @@
 local _, L = ...;
 if GetLocale() == "deDE" then
     L["Enabled"] = "Enabled";
-    L["Instantly delete your messages from the new Communities window"] = "Instantly delete your messages from the new Communities window";
+    L["Enabled for ALL channels"] = "Enabled for ALL channels";
+    L["Instantly delete your guild-messages from the new Communities window"] = "Instantly delete your guild-messages from the new Communities window";
+    L["Instantly delete your messages from  all of the new Communities windows"] = "Instantly delete your messages from  all of the new Communities windows";
     L["GuildMessageRemover"] = "GuildMessageRemover"; -- used for slashcommand and interface option panel name
 end
